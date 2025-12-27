f4565d69023a36bb7edcc5faa24c5fc7c072ac04
Elysia <118418714+elysias123@users.noreply.github.com>
2025-12-27 13:32:42 +0800

    script: 修复监听端口占用匹配错误

--- module/src/plugins/ruleconverter/post-fs-data.sh
+++ module/src/plugins/ruleconverter/post-fs-data.sh
@@ -1,6 +1,6 @@
 [ -z "$version" ] && . /data/clash/clash.config
 
-pid=$(lsof | grep ${ruleconverter_port})
+pid=$(lsof | grep :${ruleconverter_port})
 
 if [ -n "${pid}" ]; then
     kill -15 $(echo ${pid} | awk '{print $2}')
