3a89533bf5279e9af3e3de35597a5aa3dfcffda7
MT <156982948+MangTianYa@users.noreply.github.com>
2026-07-08 14:31:56 +0800

    Update action.sh

--- module/src/tools/action.sh
+++ module/src/tools/action.sh
@@ -1,11 +1,11 @@
 MODDIR=/data/adb/akashaProxy
-. ${MODDIR}/clash.env
+. ${MODDIR}/sing-box.env
 
-pid=$(curl -sL http://127.0.0.1:${kernel_ui_port} | grep mihomo)
+pid=$(curl -sL http://127.0.0.1:${kernel_ui_port} | grep sing-box)
 if [[ "${pid}" ]]; then
     echo "正在停止akashaProxy."
-    ${MODDIR}/scripts/clash.service -k && ${MODDIR}/scripts/clash.iptables -k
+    ${MODDIR}/scripts/sing-box.service -k && ${MODDIR}/scripts/sing-box.iptables -k
 else
     echo "正在启动akashaProxy."
-    ${MODDIR}/scripts/clash.service -s && ${MODDIR}/scripts/clash.iptables -s
+    ${MODDIR}/scripts/sing-box.service -s && ${MODDIR}/scripts/sing-box.iptables -s
 fi
